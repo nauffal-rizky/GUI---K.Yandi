c0264fb89681efd451200d97cac598c1507f7dad
Nauffal Nur Rizky <nauffal2005@gmail.com>
2023-06-19 13:46:04 +0700

    fourth commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mainPage = document.querySelector('.mainPage');
 const userCheck = document.querySelector('.userCheck');
-const questionPage = document.querySelector('.questionPage');
 const startBtn = document.querySelector('.startBtn');
 startBtn.addEventListener('click', () => {
   userCheck.classList.add('show-page');
@@ -8,38 +7,86 @@ startBtn.addEventListener('click', () => {
 });
 
 // USER CHECK FORM
-const ageSlide = document.querySelector('.age-slide .option');
+const ageSlide = document.querySelector('.age-slide');
+const ageOption = ageSlide.querySelector('.option');
 const ageDropdownBtn = document.querySelector('.age-slide #dropdownBtn');
-console.log(ageDropdownBtn);
 ageDropdownBtn.addEventListener('click', () => {
-  if (ageSlide.getBoundingClientRect().height === 0) {
-    ageSlide.style.height = '112px';
+  if (ageOption.getBoundingClientRect().height === 0) {
+    ageOption.style.height = '112px';
   } else {
-    ageSlide.style.height = 0;
+    ageOption.style.height = 0;
   }
 });
 
-const ageAnswer = ageSlide.querySelectorAll('span');
+// age answer
+const ageAnswer = ageOption.querySelectorAll('span');
 ageAnswer.forEach((answer) => {
   answer.addEventListener('click', () => {
-    console.log(answer.innerText);
+    const answerValue = answer.innerText;
+
+    const showAnswer = document.createElement('div');
+    showAnswer.innerHTML = `
+      <i class="fa-solid fa-circle-xmark cancelBtn"></i>
+      <span>${answerValue}</span>
+    `;
+
+    const titleContain = ageSlide.querySelector('.title-contain');
+    if (!ageSlide.classList.contains('show-answer')) {
+      ageSlide.classList.add('show-answer');
+
+      titleContain.appendChild(showAnswer);
+      ageOption.style.height = 0;
+
+      const cancelBtn = showAnswer.querySelector('.cancelBtn');
+      cancelBtn.addEventListener('click', () => {
+        ageSlide.classList.remove('show-answer');
+        titleContain.removeChild(showAnswer);
+        ageOption.style.height = 0;
+      });
+    } else {
+      console.log(answerValue);
+
+      // titleContain.removeChild(showAnswer);
+      titleContain.appendChild(showAnswer);
+    }
   });
 });
 
-const eduSlide = document.querySelector('.edu-slide .option');
+const eduSlide = document.querySelector('.edu-slide');
+const eduOption = eduSlide.querySelector('.option');
 const eduDropdownBtn = document.querySelector('.edu-slide #dropdownBtn');
 eduDropdownBtn.addEventListener('click', () => {
-  if (eduSlide.getBoundingClientRect().height === 0) {
-    eduSlide.style.height = '112px';
+  if (eduOption.getBoundingClientRect().height === 0) {
+    eduOption.style.height = '112px';
   } else {
-    eduSlide.style.height = 0;
+    eduOption.style.height = 0;
   }
 });
 
-const eduAnswer = eduSlide.querySelectorAll('span');
+// education answer
+const eduAnswer = eduOption.querySelectorAll('span');
 eduAnswer.forEach((answer) => {
   answer.addEventListener('click', () => {
-    console.log(answer.innerText);
+    const showAnswer = document.createElement('div');
+    showAnswer.innerHTML = `
+      <i class="fa-solid fa-circle-xmark cancelBtn"></i>
+      <span>${answer.innerText}</span>
+    `;
+
+    if (!eduSlide.classList.contains('show-answer')) {
+      eduSlide.classList.add('show-answer');
+
+      const titleContain = eduSlide.querySelector('.title-contain');
+      titleContain.appendChild(showAnswer);
+      eduOption.style.height = 0;
+
+      const cancelBtn = showAnswer.querySelector('.cancelBtn');
+      cancelBtn.addEventListener('click', () => {
+        eduSlide.classList.remove('show-answer');
+        titleContain.removeChild(showAnswer);
+        eduOption.style.height = 0;
+      });
+    }
   });
 });
 
